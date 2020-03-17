38d7b28d4fa85013add7ca935d9aebaa915d8aa6
Steven Phaedonos <steven@phaedonos.com>
2020-03-17 19:31:25 +1100

    Reduce scope of origin access role IAM policy

--- pipeline/iam.tf
+++ pipeline/iam.tf
@@ -2,17 +2,7 @@
 data "aws_iam_policy_document" "project_bucket_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.project_bucket.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.project_bucket.arn]
+    resources = ["${aws_s3_bucket.project_bucket.arn}/frontend/*"]
 
     principals {
       type        = "AWS"
